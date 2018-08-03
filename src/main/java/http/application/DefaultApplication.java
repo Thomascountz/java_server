954a3a5f5d68b80a9596fd2105887bd290020e58
Thomas Countz <thomascountz@gmail.com>
2018-08-03 09:49:30 -0400

    add html and styling to default app

--- src/main/java/http/application/DefaultApplication.java
+++ src/main/java/http/application/DefaultApplication.java
@@ -5,6 +5,10 @@ import http.server.RequestParams;
 public class DefaultApplication extends Application {
 
     public byte[] apply(RequestParams requestParams){
-        return "HTTP/1.1 200 OK\r\n\r\nHello, World!".getBytes();
+        if (requestParams.getPath().equals("/") && requestParams.getMethod().equals("GET")) {
+            return "HTTP/1.1 200 OK\r\n\r\n<h1 align=\"center\">Hello, World!</h1>".getBytes();
+        } else {
+            return ("HTTP/1.1 404 Not Found\r\n\r\n<h1 align=\"center\">404</h1><h2 align=\"center\">We could not find the page your were looking for.</h2>").getBytes();
+        }
     }
 }
